12a646b7500f2c36ec2fd27055e7237f6ef8ca7e
Doavail LLC <doavailllc@gmail.com>
2026-06-15 20:21:38 +0300

    Set home page as landing page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,72 +1,31 @@
-import { Routes, Route } from "react-router-dom";
-
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
-import ResortDetails from "./pages/ResortDetails";
-import BecomeHost from "./pages/BecomeHost";
-import CategorySelect from "./pages/CategorySelect";
-import Profile from "./pages/Profile";
-import Wishlist from "./pages/Wishlist";
-import Trips from "./pages/Trips";
-import Messages from "./pages/Messages";
-import Notifications from "./pages/Notifications";
-import AccountSettings from "./pages/AccountSettings";
-import Checkout from "./pages/Checkout";
-import BookingSuccess from "./pages/BookingSuccess";
-import HostDashboard from "./pages/HostDashboard";
-import SearchResults from "./pages/SearchResults";
-import HostListings from "./pages/HostListings";
-import EditListing from "./pages/EditListing";
-import HostCalendar from "./pages/HostCalendar";
-import Earnings from "./pages/Earnings";
-import Reviews from "./pages/Reviews";
-import AdminDashboard from "./pages/AdminDashboard";
-import NotFound from "./pages/NotFound";
-import HelpCenter from "./pages/HelpCenter";
-import LanguageCurrency from "./pages/LanguageCurrency";
-import Experience from "./pages/Experiences";
-import Services from "./pages/Services";
-import PaymentMethods from "./pages/PaymentMethods";
-import AddProperty from "./pages/AddProperty";
-import HostReservations from "./pages/HostReservations";
-import HostReviews from "./pages/HostReviews";
-import Analytics from "./pages/Analytics";
-import PaymentHistory from "./pages/PaymentHistory";
-import Payouts from "./pages/Payouts";
-import AdminLayout from "./layouts/AdminLayout";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import TripDetails from "./pages/TripDetails";
-import WriteReview from "./pages/WriteReview";
-import Receipt from "./pages/Receipt";
-import Security from "./pages/Security";
-import ForgotPassword from "./pages/ForgotPassword";
-import Support from "./pages/Support";
-import RecentlyViewed from "./pages/RecentlyViewed";
-import HostProfile from "./pages/HostProfile";
-import Verification from "./pages/Verification";
-import ServiceDetails from "./pages/ServiceDetails";
-import ServiceBookingDetails from "./pages/ServiceBookingDetails";
-function ProtectedPage({ children }) {
-  return <ProtectedRoute>{children}</ProtectedRoute>;
-}
-
 export default function App() {
   return (
     <Routes>
-   <Route path="/" element={<Home />} />
-<Route path="/home" element={<Home />} />
-<Route path="/login" element={<Login />} />
-<Route path="/signup" element={<Signup />} />
 
+      {/* ========= PUBLIC ROUTES ========= */}
+
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+
+      <Route path="/search-results" element={<SearchResults />} />
+      <Route path="/reserve/:id" element={<ResortDetails />} />
+      <Route path="/service/:id" element={<ServiceDetails />} />
       <Route
-        path="/home"
-        element={
-          <ProtectedPage>
-            <Home />
-          </ProtectedPage>
-        }
+        path="/service-booking/:id"
+        element={<ServiceBookingDetails />}
       />
+
+      <Route path="/experiences" element={<Experience />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/help" element={<HelpCenter />} />
+
+
+
+      {/* ========= PROTECTED ROUTES ========= */}
 
       <Route
         path="/category"
@@ -78,94 +37,10 @@ export default function App() {
       />
 
       <Route
-        path="/help"
-        element={
-          <ProtectedPage>
-            <HelpCenter />
-          </ProtectedPage>
-        }
-      />
-
-      <Route
-        path="/profile"
-        element={
-          <ProtectedPage>
-            <Profile />
-          </ProtectedPage>
-        }
-      />
-<Route
-  path="/verification"
-  element={
-    <ProtectedPage>
-      <Verification />
-    </ProtectedPage>
-  }
-/>
-      <Route
-        path="/reserve/:id"
-        element={
-          <ProtectedPage>
-            <ResortDetails />
-          </ProtectedPage>
-        }
-      />
-
-      <Route
         path="/wishlist"
         element={
           <ProtectedPage>
             <Wishlist />
-          </ProtectedPage>
-        }
-      />
-<Route
-  path="/receipt/:id"
-  element={
-    <ProtectedPage>
-      <Receipt />
-    </ProtectedPage>
-  }
-/>
-      <Route
-        path="/trips"
-        element={
-          <ProtectedPage>
-            <Trips />
-          </ProtectedPage>
-        }
-      />
-<Route
-  path="/security"
-  element={
-    <ProtectedPage>
-      <Security />
-    </ProtectedPage>
-  }
-/>
-      <Route
-        path="/messages"
-        element={
-          <ProtectedPage>
-            <Messages />
-          </ProtectedPage>
-        }
-      />
-
-      <Route
-        path="/notifications"
-        element={
-          <ProtectedPage>
-            <Notifications />
-          </ProtectedPage>
-        }
-      />
-
-      <Route
-        path="/account-settings"
-        element={
-          <ProtectedPage>
-            <AccountSettings />
           </ProtectedPage>
         }
       />
@@ -187,76 +62,48 @@ export default function App() {
           </ProtectedPage>
         }
       />
-<Route
-  path="/host/:id"
-  element={
-    <ProtectedPage>
-      <HostProfile />
-    </ProtectedPage>
-  }
-/>
+
       <Route
-        path="/language"
+        path="/trips"
         element={
           <ProtectedPage>
-            <LanguageCurrency />
+            <Trips />
           </ProtectedPage>
         }
       />
 
       <Route
-        path="/search-results"
+        path="/messages"
         element={
           <ProtectedPage>
-            <SearchResults />
+            <Messages />
           </ProtectedPage>
         }
       />
 
       <Route
-        path="/experiences"
+        path="/notifications"
         element={
           <ProtectedPage>
-            <Experience />
-          </ProtectedPage>
-        }
-      />
-<Route
-  path="/service-booking/:id"
-  element={<ServiceBookingDetails />}
-/>
-      <Route
-        path="/services"
-        element={
-          <ProtectedPage>
-            <Services />
+            <Notifications />
           </ProtectedPage>
         }
       />
 
       <Route
-        path="/reviews"
+        path="/profile"
         element={
           <ProtectedPage>
-            <Reviews />
+            <Profile />
           </ProtectedPage>
         }
       />
 
       <Route
-        path="/payment-methods"
+        path="/account-settings"
         element={
           <ProtectedPage>
-            <PaymentMethods />
-          </ProtectedPage>
-        }
-      />
-
-      <Route
-        path="/payments"
-        element={
-          <ProtectedPage>
-            <PaymentHistory />
+            <AccountSettings />
           </ProtectedPage>
         }
       />
@@ -316,19 +163,19 @@ export default function App() {
       />
 
       <Route
-        path="/host-reviews"
+        path="/earnings"
         element={
           <ProtectedPage>
-            <HostReviews />
+            <Earnings />
           </ProtectedPage>
         }
       />
 
       <Route
-        path="/earnings"
+        path="/payments"
         element={
           <ProtectedPage>
-            <Earnings />
+            <PaymentHistory />
           </ProtectedPage>
         }
       />
@@ -343,54 +190,28 @@ export default function App() {
       />
 
       <Route
-        path="/edit-listing"
+        path="/reviews"
         element={
           <ProtectedPage>
-            <EditListing />
+            <Reviews />
           </ProtectedPage>
         }
       />
-<Route
-  path="/support"
-  element={
-    <ProtectedPage>
-      <Support />
-    </ProtectedPage>
-  }
-/>
-      <Route
-  path="/trip/:id"
-  element={
-    <ProtectedPage>
-      <TripDetails />
-    </ProtectedPage>
-  }
-/>
-<Route
-  path="/review/:bookingId"
-  element={
-    <ProtectedPage>
-      <WriteReview />
-    </ProtectedPage>
-  }
-/>
-<Route path="/service/:id" element={<ServiceDetails />} />
 
-
-<Route
-  path="/recently-viewed"
-  element={
-    <ProtectedPage>
-      <RecentlyViewed />
-    </ProtectedPage>
-  }
-/>
-<Route path="/forgot-password" element={<ForgotPassword />} />
       <Route
-        path="/edit-listing/:id"
+        path="/verification"
         element={
           <ProtectedPage>
-            <EditListing />
+            <Verification />
+          </ProtectedPage>
+        }
+      />
+
+      <Route
+        path="/language"
+        element={
+          <ProtectedPage>
+            <LanguageCurrency />
           </ProtectedPage>
         }
       />
@@ -405,6 +226,7 @@ export default function App() {
       </Route>
 
       <Route path="*" element={<NotFound />} />
+
     </Routes>
   );
 }
