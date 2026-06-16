21f434e909774b81968ea26f23bf4bbad50bd64b
Doavail LLC <doavailllc@gmail.com>
2026-06-16 19:41:35 +0300

    Set home page as landing page

--- web/src/api/api.js
+++ web/src/api/api.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
+const API_URL =
+  import.meta.env.VITE_API_URL || "http://44.212.49.157:5000/api";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: API_URL,
 });
 
 api.interceptors.request.use((config) => {
-  const token =
+  const adminToken = localStorage.getItem("adminToken");
+  const userToken =
     localStorage.getItem("token") || sessionStorage.getItem("token");
+
+  const token = adminToken || userToken;
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
